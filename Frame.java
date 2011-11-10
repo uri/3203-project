98466c18e92dbd194c9397bb9cd9ca0d9d6f3fb4
Uri Gorelik <uri.gore@gmail.com>
2011-11-10 15:50:14 -0500

    Fixed some errors from the previous commit.

--- Frame.java
+++ Frame.java
@@ -1,7 +1,10 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Frame extends JFrame{
 
--- Network.java
+++ Network.java
@@ -7,12 +7,21 @@ public class Network{
 		nodelist = new ArrayList<Node>();
 	}
 	
+	public Network(int a, int b) {
+		// TODO: This contructor is incomplete, was added to solve errors
+	}
+	
 	public void addNode(Node n) {
 		nodelist.add(n);
 	}
 	
 	ArrayList<Node> getNodeList() {
 		return nodelist;
+	}
+
+	public ArrayList<Node> getSensorList() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
