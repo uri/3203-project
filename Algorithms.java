003292eb6df04f8a3930d2b0da26aeee978df8a2
unknown <ugorelik@VISTA4175-10.windows.scs.carleton.ca>
2011-11-22 18:46:50 -0500

    Finished the MST algorithm

--- Algorithms.java
+++ Algorithms.java
@@ -15,5 +15,46 @@ public class Algorithms {
 		
 		// Add it to the search list
 		searchList.add(startNode);
+
+		
+		// Find the next node that is connected to a node in the search list that has the smallest weight
+		
+		
+		
+		
+		
+		
+		while (true) {
+			Node smallest = null;
+			Node smallestParent = null;
+			for (Node n : searchList) {
+				
+				// Find the node with the smallest distance
+				for (Node neigh : n.getAllEdges()) {
+					
+					int distance = n.getWeight(neigh);
+					
+					// KLUDGE!!!!
+					if (smallest == null) smallest = neigh;
+					
+					else if (distance > 0 && n.getWeight(neigh) < n.getWeight(smallest) && !searchList.contains(smallest) ) {
+						smallest = neigh;
+						smallestParent = n;
+					}
+				}
+
+				// Add the smallest node to the list
+				
+			}
+			
+			if (smallest == null) break;
+			
+			searchList.add(smallest);
+			smallestParent.addMSTEdge(smallestParent);
+		} 
+		
+
+		
+		// At this point we have the smallest
 	}
 }
