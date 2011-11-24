b762bae308079de751ad1c6b8fd8da72994901b4
Jason Brennan <i.jasonbrennan@gmail.com>
2011-11-24 18:13:16 -0500

    Added the Stats runner class and stubbed out some disposable methods.

---
+++ StatisticsRunner.java
@@ -0,0 +1,31 @@
+
+public class StatisticsRunner {
+	
+	DirectedNetwork directedNetwork;
+	
+	public StatisticsRunner(DirectedNetwork network) {
+		super();
+		
+		directedNetwork = network;
+		
+	}
+	
+	
+	public void shortestPaths() {
+		
+	}
+	
+	
+	public void lengthOfRoutes() {
+		
+	}
+	
+	
+	public void diameterOfNetwork() {
+		
+	}
+	
+	
+	
+
+}
