1689aae868ba1d2bb449fa5c571710eb6f94861f
unknown <ugorelik@WIN7-4175-01.windows.scs.carleton.ca>
2011-11-24 19:34:44 -0500

    Antenna Orienation now works

--- DirectedNetwork.java
+++ DirectedNetwork.java
@@ -7,7 +7,7 @@ public class DirectedNetwork extends Network{
 		sensorlist = new ArrayList<Node>();
 		//copy relevant data from the base network
 		Node temp;
-		System.out.println("\nNew Directed Network Created");
+		//System.out.println("\nNew Directed Network Created");
 		for (Node s: n.getSensorList()){
 			temp = new Node(s.getX(),s.getY());
 			sensorlist.add(temp);
@@ -54,17 +54,18 @@ public class DirectedNetwork extends Network{
 	//Eric's Algorithm
 	public void AntennaOrientation(){
 		for (Node n: sensorlist){
-			System.out.println("SENSOR LIST. This node has " + n.getMSTEdges().size() + " edges and " + n.getAllEdges().size() + " neighbours" );
+			//System.out.println("SENSOR LIST. This node has " + n.getMSTEdges().size() + " edges and " + n.getAllEdges().size() + " neighbours" );
 			double smallestAngle = 1000;
 			double largestAngle = 0;
 			double currentAngle = 0;
+			double secondSmallestAngle = 361;
 			double x;
 			for (Node s: n.getMSTEdges()){
-				System.out.println("A node in the neighbourlist");
+				//System.out.println("A node in the neighbourlist");
 				currentAngle = 0;
 				double deltaX = (s.getX() - n.getX());
 				double deltaY = (s.getY() - n.getY());
-				System.out.println("Delta X: " + deltaX + ". DeltaY: " + deltaY);
+				//System.out.println("Delta X: " + deltaX + ". DeltaY: " + deltaY);
 				if (deltaX == 0 && deltaY>0){
 					currentAngle = 270;
 				}
@@ -89,21 +90,28 @@ public class DirectedNetwork extends Network{
 					x = deltaY/deltaX;
 					currentAngle = 360 - Math.toDegrees(Math.atan(x));
 				}
-				System.out.println("current angle is " + currentAngle);
+				//System.out.println("current angle is " + currentAngle);
 				
 				if (currentAngle < smallestAngle)
 					smallestAngle =currentAngle;
 				if (currentAngle > largestAngle)
 					largestAngle = currentAngle;
+				if(currentAngle < secondSmallestAngle && currentAngle > smallestAngle)
+					secondSmallestAngle = currentAngle;
 			}
+			System.out.println("Second Smallest Angle: " + secondSmallestAngle);
 			//if statement ensures the smallest possible angle is drawn
-		/*	if (largestAngle-smallestAngle > 180){
-				n.setDirection(largestAngle);
-				n.setAngle(360+smallestAngle-largestAngle);
+			if (secondSmallestAngle == 361)
+				secondSmallestAngle = largestAngle;
+			if (secondSmallestAngle-smallestAngle > 180){
+				n.setDirection(secondSmallestAngle);
+				n.setAngle(360+smallestAngle-secondSmallestAngle);
+				System.out.println("Doing This!");
 			}
-			else{*/
+			else{
 				n.setDirection(smallestAngle);
 				n.setAngle(largestAngle-smallestAngle);
+			}
 		}
 	}
 
--- Network.java
+++ Network.java
@@ -25,7 +25,6 @@ public class Network{
 		Node temp = new Node();
 		Random rand = new Random();
 		sensorlist = new ArrayList<Node>();
-		System.out.println("\nNew Network with Strength " + strength);
 		//starting node
 		sensorlist.add(new Node(DEFAULTX,DEFAULTY));
 		for (int i = 1; i < n; i++)
@@ -33,7 +32,6 @@ public class Network{
 			x = rand.nextInt(i);//selects a random node to place the new node near
 			temp = sensorlist.get(x);
 			int max = (int)Math.sqrt(strength*strength/2);//the maximum distance in either direction from any other node
-			int newX=strength, newY=strength;
 			
 			Node newNode = null;
 			// Make sure we don't add it twice
@@ -42,8 +40,6 @@ public class Network{
 				newNode = new Node(
 						temp.getX()+rand.nextInt(max),
 						temp.getY()+rand.nextInt(max));
-				System.out.println("Base Node: " + temp.getX() + "," + temp.getY());
-				System.out.println("New Node :" + newNode.getX() + "," + newNode.getY());
 			}
 			sensorlist.add(newNode);
 		}
