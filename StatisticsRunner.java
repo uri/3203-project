8d087ae1083950a29334e1fc6f2f19d7ab3476f2
Jason Brennan <i.jasonbrennan@gmail.com>
2011-11-27 17:36:01 -0500

    Refactored some of the stats methods. Moved my code for diameter into the lengthOfRoutes method because I had gotten confused before. Implemented diameter properly now. Just need to refactor a few more things and its all good.

--- StatisticsRunner.java
+++ StatisticsRunner.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class StatisticsRunner {
 	
-	protected List<Network> listOfNetworks;
-	protected List<DirectedNetwork> listOfDirectedNetworks;
+	protected ArrayList<Network> listOfNetworks;
+	protected ArrayList<DirectedNetwork> listOfDirectedNetworks;
 	
 	public StatisticsRunner(int numberOfNetworks, int numberOfNodesPerNetwork, int signalStrength) {
 		super();
@@ -28,41 +28,80 @@ public class StatisticsRunner {
 	
 	public void shortestPaths() {
 		if (listOfDirectedNetworks.size() < 1) return; // no funny business..
-		System.out.println("Running shortest paths on " + listOfDirectedNetworks.size() + " directed networks...");
 		
+		
+		//System.out.println("Running shortest paths on " + listOfNetworks.size() + "  networks...");
+		//System.out.println("Done running shortest paths. Average nodes in shortest-path: " + averageOfShortestPathsForNetwork(listOfNetworks));
+		
+		System.out.println("Running shortest paths on " + listOfDirectedNetworks.size() + " directed networks...");
+		System.out.println("Done running shortest paths. Average nodes in shortest-path: " + averageOfShortestPathsForNetwork(listOfDirectedNetworks));
+	}
+	
+	
+	private double averageOfShortestPathsForNetwork(ArrayList<? extends Network> networks) {
 		int runningTotal = 0;
 		
-		for (DirectedNetwork net : listOfDirectedNetworks) {
+		for (Network net : networks) {
 			Node start = net.getSensorList().get(0);
 			Node end = net.getSensorList().get(net.getSensorList().size() - 1);
 			net.shortestPath(start, end);
 			
 			List<Node> shortestPath = net.getShortestPathList();
-			System.out.println("Nodes in the shortest path...printing " + shortestPath.size() + " nodes");
-			for (Node node : shortestPath) {
-				System.out.println(node.toString());
-			}
 			runningTotal += shortestPath.size();
 		}
-		double average = (double)runningTotal/(double)listOfDirectedNetworks.size();
-		System.out.println("Done running shortest paths. Average nodes in shortest-path: " + average);
+		return (double)runningTotal/(double)networks.size();
+	}
+	
+	
+	public void diameterOfNetwork() {
+		// find the shortest paths between every pair of nodes in the network
+		// then pick the longest one.
+		// that's the diameter
+		// c.f. http://stackoverflow.com/questions/3174569/what-is-meant-by-diameter-of-a-network
+		
+		for (Network net : listOfDirectedNetworks) {
+			
+			int currentLongestPath = 0;
+			for (Node startNode : net.getSensorList()) {
+				ArrayList<Node> allOtherNodes = new ArrayList<Node>(net.getSensorList());
+				allOtherNodes.remove(startNode);
+				
+				// iterate over all the other nodes
+				for (Node endNode : allOtherNodes) {
+					// get the shortest path between them.
+					
+					net.shortestPath(startNode, endNode);
+					List<Node> shortestPath = net.getShortestPathList();
+					if (shortestPath.size() > currentLongestPath)
+						currentLongestPath = shortestPath.size();
+				}
+			}
+			
+			System.out.println("The longest shortest path for this network is: " + currentLongestPath);
+			
+		}
+		
 	}
 	
 	
 	public void lengthOfRoutes() {
 		
+		if (listOfDirectedNetworks.size() < 1) return;
+		
+		System.out.println("Running lengthOfNetwork on " + listOfDirectedNetworks.size() + " omnidirectional networks...");
+		System.out.println("Done running diameters. Average greatest distance is: " + averageLengthOfNetworkList(listOfNetworks));
+		
+		
+		System.out.println("Running lengthOfNetwork on " + listOfDirectedNetworks.size() + " directed networks...");
+		System.out.println("Done running diameters. Average greatest distance is: " + averageLengthOfNetworkList(listOfDirectedNetworks));
+		
 	}
 	
 	
-	public void diameterOfNetwork() {
-		
-		if (listOfDirectedNetworks.size() < 1) return;
-		
-		System.out.println("Running diameterOfNetwork on " + listOfDirectedNetworks.size() + " directed networks...");
-		
+	private double averageLengthOfNetworkList(ArrayList<? extends Network> networks) {
 		
 		int runningDistanceTotal = 0;
-		for (DirectedNetwork net : listOfDirectedNetworks) {
+		for (Network net : networks) {
 			Node start = net.getSensorList().get(0);
 			Node end = net.getSensorList().get(net.getSensorList().size() - 1);
 			net.shortestPath(start, end);
@@ -74,17 +113,12 @@ public class StatisticsRunner {
 				if (node.getDistance() > biggest.getDistance())
 					biggest = node;
 			}
+			
 			runningDistanceTotal += biggest.getDistance();
-			System.out.println("The biggest distance in this network is " + biggest.getDistance());
 			
 		}
-		double average = (double)runningDistanceTotal/(double)listOfDirectedNetworks.size();
-		System.out.println("Done running diameters. Average greatest distance is: " + average);
-		
-		
+		return (double)runningDistanceTotal/(double)networks.size();
 	}
-	
-	
 	
 
 }
